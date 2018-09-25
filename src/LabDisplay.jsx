d1ef63bc88797ecb55d49b2b9fb5e1a5ede7421e
JC Estibariz <jestibariz@bluecatnetworks.com>
2018-09-25 18:34:27 -0400

    Merge Lab display components

--- src/LabDisplay.jsx
+++ src/LabDisplay.jsx
@@ -1,9 +1,9 @@
 import {h} from 'preact';
 import PropTypes from 'prop-types';
 
-import ABPlane from './ABPlane';
-import LPlane from './LPlane';
-import CPlane from './CPlane';
+const getABStyle = color => ({left: 160 + color.a, top: 160 - color.b, backgroundColor: color.hex});
+const getLStyle = color => ({top: 260 - 2 * color.l, backgroundColor: color.hex});
+const getCStyle = color => ({left: 25 + 2 * color.c, backgroundColor: color.hex});
 
 const LabDisplay = ({palette}) => {
 	const colors = palette.map(e => {
@@ -12,11 +12,25 @@ const LabDisplay = ({palette}) => {
 		const hex = e.hex();
 		return {l, a, b, c, hex};
 	});
+
 	return (
 		<div className="LabDisplay">
-			<ABPlane palette={colors}/>
-			<LPlane palette={colors}/>
-			<CPlane palette={colors}/>
+			<div className="ABPlane">
+				<div className="ABPlane__x"/>
+				<div className="ABPlane__y"/>
+				<div className="ABPlane__label">a*b*</div>
+				{colors.map(c => <div className="ABPlane__dot" style={getABStyle(c)}/>)}
+			</div>
+
+			<div className="LPlane">
+				<div className="LPlane__label">L*</div>
+				{colors.map(c => <div className="LPlane__dot" style={getLStyle(c)}/>)}
+			</div>
+
+			<div className="CPlane">
+				<div className="CPlane__label">c*</div>
+				{colors.map(c => <div className="CPlane__dot" style={getCStyle(c)}/>)}
+			</div>
 		</div>
 	);
 };
