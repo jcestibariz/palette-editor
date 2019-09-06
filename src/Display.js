a8bdeca8dc2d59891c0487ce93d7eb06668b2950
JC Estibariz <mg05182-gh@yahoo.ca>
2019-09-05 21:47:45 -0400

    Replace DeltaE with WCAG contrast

--- src/Display.js
+++ src/Display.js
@@ -1,23 +1,27 @@
 import preact from 'preact';
 import PropTypes from 'prop-types';
-import chroma, {deltaE} from 'chroma-js';
+import chroma, {contrast} from 'chroma-js';
 
 import formatNumber from './formatNumber';
 
 const getColor = bg => (bg.luminance() < 0.5 ? '#ffffff' : '#000000');
 
+const renderContrast = c => formatNumber(c) + ' ' + (c >= 7 ? 'AAA' : c >= 4.5 ? 'AA' : c >= 3 ? 'G' : 'F');
+
 const Display = ({palette, bg, current, onSelect}) => (
 	<div className="Display" style={{color: getColor(bg), backgroundColor: bg.hex()}}>
-		{palette.map(c => chroma.lch(c)).map((c, i, p) => (
-			<div className="Display__color">
-				<div
-					className={'Display__bar' + (i === current ? ' Display--current' : '')}
-					style={{backgroundColor: c.hex()}}
-					onClick={() => onSelect(i)}
-				/>
-				<div className="Display__deltaE">{i !== current && formatNumber(deltaE(c, p[current]))}</div>
-			</div>
-		))}
+		{palette
+			.map(c => chroma.lch(c))
+			.map((c, i) => (
+				<div className="Display__color">
+					<div
+						className={'Display__bar' + (i === current ? ' Display--current' : '')}
+						style={{backgroundColor: c.hex()}}
+						onClick={() => onSelect(i)}
+					/>
+					<div className="Display__contrast">{renderContrast(contrast(c, bg))}</div>
+				</div>
+			))}
 	</div>
 );
 
