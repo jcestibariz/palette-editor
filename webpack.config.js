5af694b71384cf90560dc76e7ba2cee5e4d0433d
JC Estibariz <mg05182-gh@yahoo.ca>
2021-09-29 15:01:48 -0400

    Use babel.config.json

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,7 @@ module.exports = {
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
-				use: [
-					{
-						loader: 'babel-loader',
-						options: {
-							cacheDirectory: true,
-							presets: [
-								['@babel/env', {modules: false, targets: {browsers: 'chrome >= 88'}}],
-								['@babel/react', {runtime: 'automatic', importSource: 'preact', useSpread: true}],
-							],
-							plugins: ['@babel/proposal-class-properties'],
-						},
-					},
-				],
+				use: [{loader: 'babel-loader', options: {cacheDirectory: true}}],
 			},
 		],
 	},
